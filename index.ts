d43d6bb54c30e1e1c8e8fc3ce8a96c18c8edd189
Vadim Cherepanov <xenos26@gmail.com>
2023-09-10 20:55:47 +0700

    feat: add server with figlet

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3001,
+  fetch(req) {
+    const body = figlet.textSync(`Bun!`)
+    return new Response(body);
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
+
